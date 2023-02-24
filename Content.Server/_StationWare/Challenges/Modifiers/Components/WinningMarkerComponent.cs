2fb8dd51be079bf5d0c52f3d26c58171b8a80be9
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2023-02-24 07:13:43 -0500

    keep the crown challenge (#48)

---
+++ Content.Server/_StationWare/Challenges/Modifiers/Components/WinningMarkerComponent.cs
@@ -0,0 +1,21 @@
+﻿namespace Content.Server._StationWare.Challenges.Modifiers.Components;
+
+/// <summary>
+/// This is used for marking a player as the winner of a challenge.
+/// Hitting them causes the crown to transfer to that player.
+/// </summary>
+[RegisterComponent]
+public sealed class WinningMarkerComponent : Component
+{
+    /// <summary>
+    /// The challenge that this winner belongs to
+    /// </summary>
+    [DataField("challenge")]
+    public EntityUid Challenge;
+
+    /// <summary>
+    /// The effect marker
+    /// </summary>
+    [DataField("marker")]
+    public EntityUid? Marker;
+}
