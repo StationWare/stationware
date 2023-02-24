5cb88acf7c21ac1341074ca3f034c9333beb2a1d
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2023-02-24 07:14:03 -0500

    find the fake (ian/corgis) (#49)

---
+++ Content.Server/_StationWare/Challenges/Modifiers/Components/InteractionWinComponent.cs
@@ -0,0 +1,15 @@
+﻿namespace Content.Server._StationWare.Challenges.Modifiers.Components;
+
+/// <summary>
+/// This is for entities that make you win/lose a challenge
+/// when you interact with them.
+/// </summary>
+[RegisterComponent]
+public sealed class InteractionWinComponent : Component
+{
+    /// <summary>
+    /// Whether or not you win or lose on interact
+    /// </summary>
+    [DataField("winOnInteract")]
+    public bool WinOnInteract = true;
+}
---
+++ Content.Server/_StationWare/Challenges/Modifiers/Systems/InteractionWinSystem.cs
@@ -0,0 +1,27 @@
+﻿using Content.Server._StationWare.Challenges.Modifiers.Components;
+using Content.Shared.Interaction;
+
+namespace Content.Server._StationWare.Challenges.Modifiers.Systems;
+
+public sealed class InteractionWinSystem : EntitySystem
+{
+    [Dependency] private readonly StationWareChallengeSystem _stationWareChallenge = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<InteractionWinComponent, InteractHandEvent>(OnInteractHand);
+    }
+
+    private void OnInteractHand(EntityUid uid, InteractionWinComponent component, InteractHandEvent args)
+    {
+        var player = args.User;
+        foreach (var challenge in EntityQuery<StationWareChallengeComponent>())
+        {
+            var challengeEnt = challenge.Owner;
+            if (!challenge.Participants.ContainsValue(player))
+                continue;
+            _stationWareChallenge.SetPlayerChallengeState(player, challengeEnt, component.WinOnInteract, challenge);
+        }
+    }
+}
