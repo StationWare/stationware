d44a3d086f60fe3d72f21f84d2a00c05277f3870
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2023-02-24 06:49:10 -0500

    King of the hill (#50)

---
+++ Content.Server/_StationWare/Challenges/Modifiers/Components/ControlPointModifierComponent.cs
@@ -0,0 +1,18 @@
+﻿namespace Content.Server._StationWare.Challenges.Modifiers.Components;
+
+/// <summary>
+/// This is used for designating an area as a control point.
+/// Standing in a control point during a ControlPointModifier
+/// will make you win.
+/// </summary>
+[RegisterComponent]
+public sealed class ControlPointComponent : Component
+{
+
+}
+
+[RegisterComponent]
+public sealed class ControlPointModifierComponent : Component
+{
+
+}
--- Resources/Locale/en-US/_StationWare/ware-events/challenges.ftl
+++ Resources/Locale/en-US/_StationWare/ware-events/challenges.ftl
@@ -1,2 +1,3 @@
 challenge-freeze = Freeze! Don't move!
+challenge-king-of-the-hill = End the challenge inside the circle.
 challenge-one-shot-gun = One gun. One bullet. One chance. Don't let the person shot be you. Survive to win.
