610e99ec3ee8d878894f5adcbf95e69256a42369
EmoGarbage404 <retron404@gmail.com>
2023-02-23 22:32:48 -0500

    Freeze Challenge

---
+++ Content.Server/_StationWare/Challenges/Modifiers/Components/FreezeModifierComponent.cs
@@ -0,0 +1,22 @@
+﻿namespace Content.Server._StationWare.Challenges.Modifiers.Components;
+
+/// <summary>
+/// Players musn't move or else they lose.
+/// </summary>
+[RegisterComponent]
+public sealed class FreezeModifierComponent : Component
+{
+}
+
+/// <summary>
+/// A player that is playing <see cref="FreezeModifierComponent"/>
+/// </summary>
+[RegisterComponent]
+public sealed class FreezePlayerComponent : Component
+{
+    /// <summary>
+    /// The challenge entity for the freeze modifier player
+    /// </summary>
+    [DataField("challenge")]
+    public EntityUid Challenge;
+}
--- Resources/Locale/en-US/_StationWare/ware-events/challenges.ftl
+++ Resources/Locale/en-US/_StationWare/ware-events/challenges.ftl
@@ -1,1 +1,2 @@
+challenge-freeze = Freeze! Don't move!
 challenge-one-shot-gun = One gun. One bullet. One chance. Don't let the person shot be you. Survive to win.
