29e9d9238e6f00b1aa87daacc4e792630010d977
EmoGarbage404 <retron404@gmail.com>
2023-03-04 22:14:51 -0500

    The Button Challenge

---
+++ Content.Server/_StationWare/Challenges/Modifiers/Systems/GibOnFailModifierSystem.cs
@@ -0,0 +1,23 @@
+﻿using Content.Server._StationWare.Challenges.Modifiers.Components;
+using Content.Server.Body.Systems;
+
+namespace Content.Server._StationWare.Challenges.Modifiers.Systems;
+
+public sealed class GibOnFailModifierSystem : EntitySystem
+{
+    [Dependency] private readonly BodySystem _body = default!;
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<GibOnFailModifierComponent, PlayerChallengeStateSetEvent>(OnChallengeStateSet);
+    }
+
+    private void OnChallengeStateSet(EntityUid uid, GibOnFailModifierComponent component, ref PlayerChallengeStateSetEvent args)
+    {
+        if (args.Won)
+            return;
+        if (args.Player.AttachedEntity is not { } ent)
+            return;
+        _body.GibBody(ent, true, deleteItems: true);
+    }
+}
--- Resources/Locale/en-US/_StationWare/ware-events/challenges.ftl
+++ Resources/Locale/en-US/_StationWare/ware-events/challenges.ftl
@@ -5,6 +5,7 @@ challenge-find-ian = Ian is hidden in the swarm of corgis. Pet him to win.
 challenge-exploding-tile = Don't stand on the bright red tiles.
 challenge-shootout = Total shootout! Stay alive!
 challenge-xeno-queen = Spend some time with the Xeno Queen. Stay alive!
+challenge-the-button = Quickly, press the button! But will you survive?
 challenge-wizard-duel = Wizard duel! Fight!
 challenge-one-shot-gun = One gun and one bullet. Don't get shot.
 challenge-keep-the-crown = Punch the person with the crown to steal it. The person with the crown wins. 
