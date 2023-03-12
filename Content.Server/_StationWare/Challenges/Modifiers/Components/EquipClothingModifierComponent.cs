bfb5e79fb7cffa394fc53149b594301aa8bec9df
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2023-03-12 17:00:22 -0400

    Clothing modifier + godmode modifier (#121)

---
+++ Content.Server/_StationWare/Challenges/Modifiers/Components/EquipClothingModifierComponent.cs
@@ -0,0 +1,29 @@
+﻿using Content.Shared.Storage;
+
+namespace Content.Server._StationWare.Challenges.Modifiers.Components;
+
+/// <summary>
+/// This is used for equiping players
+/// with clothing during a challenge.
+/// </summary>
+[RegisterComponent]
+public sealed class EquipClothingModifierComponent : Component
+{
+    /// <summary>
+    /// What entities will be spawned at the bus entrance
+    /// </summary>
+    [DataField("spawns", required: true)]
+    public List<EntitySpawnEntry> Spawns = default!;
+
+    /// <summary>
+    /// Tracks what we've actually spawned
+    /// </summary>
+    [DataField("spawnedEntities"), ViewVariables(VVAccess.ReadWrite)]
+    public HashSet<EntityUid> SpawnedEntities = new();
+
+    /// <summary>
+    /// What percentage of players are receiving the clothing?
+    /// </summary>
+    [DataField("receivingPercentage")]
+    public float ReceivingPercentage = 1f;
+}
---
+++ Content.Server/_StationWare/Challenges/Modifiers/Components/GodmodeModifierComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Content.Server._StationWare.Challenges.Modifiers.Components;
+
+/// <summary>
+/// This is used for giving players godmode
+/// </summary>
+[RegisterComponent]
+public sealed class GodmodeModifierComponent : Component
+{
+
+}
