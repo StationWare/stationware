98dcc575df5f86116a51b558a74ce307228f8880
Kara <lunarautomaton6@gmail.com>
2023-03-11 22:14:00 -0600

    me add new entity

---
+++ Content.Server/_StationWare/Challenges/Modifiers/Components/SpawnBlockMarkerComponent.cs
@@ -0,0 +1,15 @@
+﻿using Content.Server._StationWare.Challenges.Modifiers.Systems;
+
+namespace Content.Server._StationWare.Challenges.Modifiers.Components;
+
+/// <summary>
+///     Prevents <see cref="SpawnEntityModifierSystem"/> from spawning entities on areas mapped with this marker.
+///     This is for 'out of play' areas which are nonetheless visible, such as on Magma Factory.
+/// </summary>
+/// <remarks>
+///     This only works on anchored entities, since tile-bound.
+/// </remarks>
+[RegisterComponent]
+public sealed class SpawnBlockMarkerComponent : Component
+{
+}
