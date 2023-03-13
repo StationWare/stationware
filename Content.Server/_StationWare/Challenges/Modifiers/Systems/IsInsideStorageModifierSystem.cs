36c6052fbe479b35a9a8a8c8a42b8bc1e4236a8f
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2023-03-13 18:07:12 +0800

    Hide in locker

---
+++ Content.Server/_StationWare/Challenges/Modifiers/Systems/IsInsideStorageModifierSystem.cs
@@ -0,0 +1,30 @@
+﻿using Content.Server._StationWare.Challenges.Modifiers.Components;
+using Robust.Server.Containers;
+
+namespace Content.Server._StationWare.Challenges.Modifiers.Systems;
+
+/// <summary>
+/// This handles checking all players whether they're inside of an entity storage.
+/// </summary>
+public sealed class IsInsideStorageModifierSystem : EntitySystem
+{
+    [Dependency] private readonly ContainerSystem _containerSystem = default!;
+    [Dependency] private readonly StationWareChallengeSystem _stationWare = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<IsInsideStorageModifierComponent, BeforeChallengeEndEvent>(BeforeChallengeEndEvent);
+    }
+
+    private void BeforeChallengeEndEvent(EntityUid uid, IsInsideStorageModifierComponent component,
+        ref BeforeChallengeEndEvent args)
+    {
+        foreach (var player in args.Players)
+        {
+            if (_containerSystem.IsEntityInContainer(player))
+                _stationWare.SetPlayerChallengeState(player, uid, true);
+        }
+    }
+}
+
