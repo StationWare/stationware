cc39d2c549e67e28dcf0a6d95779b4f937374dd4
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2023-03-12 22:02:40 -0400

    Death Ball Challenge (#122)
    * Death Ball Challenge
    
    * Update death_ball.yml

---
+++ Content.Server/_StationWare/Body/GibOnCollideSystem.cs
@@ -0,0 +1,27 @@
+﻿using Content.Server.Body.Systems;
+using Content.Shared.Body.Components;
+using Content.Shared.Mobs.Components;
+using Robust.Shared.Physics.Events;
+
+namespace Content.Server._StationWare.Body;
+
+public sealed class GibOnCollideSystem : EntitySystem
+{
+    [Dependency] private readonly BodySystem _body = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<GibOnCollideComponent, StartCollideEvent>(OnCollide);
+    }
+
+    private void OnCollide(EntityUid uid, GibOnCollideComponent component, ref StartCollideEvent args)
+    {
+        var otherEnt = args.OtherFixture.Body.Owner;
+        if (!HasComp<MobStateComponent>(otherEnt) || !TryComp<BodyComponent>(otherEnt, out var body))
+            return;
+        _body.GibBody(otherEnt, true, body, true);
+        if (!component.AllowMultipleHits)
+            Del(uid);
+    }
+}
---
+++ Content.Server/_StationWare/Physics/BouncyComponent.cs
@@ -0,0 +1,14 @@
+﻿namespace Content.Server._StationWare.Physics;
+
+[RegisterComponent]
+public sealed class BouncyComponent : Component
+{
+    [DataField("minLinearVelocity")]
+    public float MinLinearVelocity = 1.5f;
+
+    [DataField("maxLinearVelocity")]
+    public float MaxLinearVelocity = 1.5f;
+
+    [DataField("angularVelocity")]
+    public float AngularVelocity = 12f;
+}
