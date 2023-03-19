0c1592f26399bbe1543f79348ac2d6ac76777dde
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2023-03-19 12:54:01 -0400

    3 New Challenges (#154)
    * 3 New Challenges
    
    Say ___, break a barrel, don't stop moving
    
    * fix license

---
+++ Content.Server/_StationWare/Challenges/Modifiers/Systems/KeepMovingModifierSystem.cs
@@ -0,0 +1,42 @@
+﻿using Content.Server._StationWare.Challenges.Modifiers.Components;
+using Robust.Shared.Physics.Components;
+
+namespace Content.Server._StationWare.Challenges.Modifiers.Systems;
+
+public sealed class KeepMovingModifierSystem : EntitySystem
+{
+    [Dependency] private readonly StationWareChallengeSystem _stationWareChallenge = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<KeepMovingModifierComponent, ChallengeStartEvent>(OnChallengeStart);
+        SubscribeLocalEvent<KeepMovingPlayerComponent, PlayerChallengeStateSetEvent>(OnChallengeStateSet);
+    }
+
+    private void OnChallengeStart(EntityUid uid, KeepMovingModifierComponent component, ref ChallengeStartEvent args)
+    {
+        foreach (var player in args.Players)
+        {
+            EnsureComp<KeepMovingPlayerComponent>(player).Challenge = uid;
+        }
+    }
+
+    private void OnChallengeStateSet(EntityUid uid, KeepMovingPlayerComponent component, ref PlayerChallengeStateSetEvent args)
+    {
+        RemComp(uid, component);
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var enumerator = EntityQueryEnumerator<KeepMovingPlayerComponent, PhysicsComponent>();
+        while (enumerator.MoveNext(out var uid, out var keepMoving, out var physics))
+        {
+            if (physics.LinearVelocity != Vector2.Zero)
+                continue;
+            _stationWareChallenge.SetPlayerChallengeState(uid, keepMoving.Challenge, false);
+        }
+    }
+}
---
+++ Content.Server/_StationWare/Challenges/Modifiers/Systems/SayPhraseModifierSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+using Content.Server._StationWare.Challenges.Modifiers.Components;
+using Content.Server.Chat.Systems;
+
+namespace Content.Server._StationWare.Challenges.Modifiers.Systems;
+
+public sealed class SayPhraseModifierSystem : EntitySystem
+{
+    [Dependency] private readonly StationWareChallengeSystem _stationWareChallenge = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<SayPhraseModifierComponent, ChallengeStartEvent>(OnChallengeStart);
+        SubscribeLocalEvent<SayPhrasePlayerComponent, PlayerChallengeStateSetEvent>(OnChallengeStateSet);
+        SubscribeLocalEvent<SayPhrasePlayerComponent, EntitySpokeEvent>(OnTransformSpeech);
+    }
+
+    private void OnChallengeStart(EntityUid uid, SayPhraseModifierComponent component, ref ChallengeStartEvent args)
+    {
+        foreach (var player in args.Players)
+        {
+            EnsureComp<SayPhrasePlayerComponent>(player).Challenge = uid;
+        }
+    }
+
+    private void OnChallengeStateSet(EntityUid uid, SayPhrasePlayerComponent component, ref PlayerChallengeStateSetEvent args)
+    {
+        RemComp(uid, component);
+    }
+
+    private void OnTransformSpeech(EntityUid uid, SayPhrasePlayerComponent component, EntitySpokeEvent args)
+    {
+        if (!TryComp<SayPhraseModifierComponent>(component.Challenge, out var modifier))
+            return;
+
+        var modifiedSpeech = args.Message.ToLower();
+        var target = modifier.Phrase.ToLower();
+        var contains = Regex.IsMatch(modifiedSpeech, "(?=.*("+target+"))");
+        if (!contains && !modifier.WrongPhraseFail)
+            return;
+        _stationWareChallenge.SetPlayerChallengeState(uid, component.Challenge, contains);
+    }
+}
