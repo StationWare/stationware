fbe1a4e343dbb0dedc65a991921e8dd0f1fd40c2
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2023-03-28 06:27:26 -0400

    stop people getting stuck in walls (#171)

---
+++ Content.Server/_StationWare/Challenges/Modifiers/Systems/ResetPositionModifierSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Content.Server._StationWare.Challenges.Modifiers.Components;
+using Content.Server.Spawners.Components;
+using Robust.Server.GameObjects;
+using Robust.Shared.Random;
+
+namespace Content.Server._StationWare.Challenges.Modifiers.Systems;
+
+public sealed class ResetPositionModifierSystem : EntitySystem
+{
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly TransformSystem _transform = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<ResetPositionModifierComponent, ChallengeEndEvent>(OnChallengeEnd);
+    }
+
+    private void OnChallengeEnd(EntityUid uid, ResetPositionModifierComponent component, ref ChallengeEndEvent args)
+    {
+        var validSpawns = EntityQuery<SpawnPointComponent, TransformComponent>()
+            .Where(p => p.Item1.SpawnType == SpawnPointType.LateJoin)
+            .Select(p => p.Item2).ToList();
+
+        foreach (var player in args.Players)
+        {
+            var spawn = _random.Pick(validSpawns).Coordinates;
+            _transform.SetCoordinates(player, spawn);
+        }
+    }
+}
