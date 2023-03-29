493bb81f513eb7601613f148a7f976ef234d8a2e
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2023-03-29 19:03:17 +0800

    math challenge

---
+++ Content.Server/_StationWare/Challenges/Modifiers/Components/MathModifierComponent.cs
@@ -0,0 +1,16 @@
+﻿namespace Content.Server._StationWare.Challenges.Modifiers.Components;
+
+[RegisterComponent]
+public sealed class MathModifierComponent : Component
+{
+    [DataField("minRange")] public int MinimumRange = 1;
+    [DataField("maxRange")] public int MaximumRange = 10;
+    public float Answer = 0;
+}
+
+[RegisterComponent]
+public sealed class MathPlayerComponent : Component
+{
+    [DataField("challenge")] public EntityUid Challenge;
+}
+
