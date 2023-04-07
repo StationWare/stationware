3f64506571667654def02bc407b5546f4eb99f12
EmoGarbage404 <retron404@gmail.com>
2023-04-06 22:33:05 -0400

    Map loading challenge modifier

---
+++ Content.Server/_StationWare/Challenges/Modifiers/Components/LoadMapModifierComponent.cs
@@ -0,0 +1,25 @@
+﻿using Robust.Shared.Map;
+using Robust.Shared.Utility;
+
+namespace Content.Server._StationWare.Challenges.Modifiers.Components;
+
+[RegisterComponent]
+public sealed class LoadMapModifierComponent : Component
+{
+    /// <summary>
+    /// Relative directory path to the given map, i.e. `Maps/ChallengeMaps/map-test.yml`
+    /// </summary>
+    [DataField("mapPath", required: true)]
+    public ResourcePath MapPath { get; } = default!;
+
+    /// <summary>
+    /// The map created by the modifier
+    /// </summary>
+    public MapId? Map;
+}
+
+[RegisterComponent]
+public sealed class MapPlayerSpawnerComponent : Component
+{
+
+}
